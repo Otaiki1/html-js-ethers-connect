b3765cb24dda3eaa5955cd3e2f1c09ebbad53e34
Otaiki1 <otaikisadiq2804@gmail.com>
2022-11-07 16:05:52 +0100

    revert to external JS , install neccessary modules

---
+++ index.js
@@ -0,0 +1,6 @@
+async function connect(){
+    if(typeof window.ethereum != undefined){
+        await window.ethereum.request({method: "eth_requestAccounts"});
+        document.getElementById("connect-button").innerHTML = "Connected"
+    }
+}
